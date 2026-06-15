faf829d12a4e6a1e4a185f404540926c1438f0ea
Dev-ali94 <sheikhaliimran5452@gmail.com>
2026-06-15 13:45:41 +0500

    changes

--- app/test/page.jsx
+++ app/test/page.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 const Page = () => {
     return (
-        <script src="http://localhost:3000/widget.js" data-id="98009cb3-b3eb-4cfc-8565-b2367e539c31" defer></script>
+        <script src="https://customer-support-chat-bot-one.vercel.app/widget.js" data-id="98009cb3-b3eb-4cfc-8565-b2367e539c31" defer></script>
     )
 }
 
