c398915c22b4668fbab84a31da964954a91003ba
Dev-ali94 <sheikhaliimran5452@gmail.com>
2026-07-16 15:42:30 +0500

    something deleted

--- app/test/page.jsx
+++ app/test/page.jsx
@@ -1,11 +1,9 @@
-
 import Script from 'next/script'
 import React from 'react'
 
 const Page = () => {
     return (
-    <script src="https://customer-support-chat-bot-one.vercel.app/widget.js" data-id="a27d082d-b238-4885-bcb9-5d4fd81faf5e" defer></script>
-       
+    <Script src="https://customer-support-chat-bot-one.vercel.app/widget.js" data-id="a27d082d-b238-4885-bcb9-5d4fd81faf5e" defer></Script> 
     )
 }
 
